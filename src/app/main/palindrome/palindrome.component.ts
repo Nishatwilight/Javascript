1342c5a27d972eb24df7dee85abb022e7a52c788
Nishatwilight <nisha.manibalan@twilightsoftwares.com>
2023-04-04 10:10:21 +0530

    work

--- src/app/main/palindrome/palindrome.component.ts
+++ src/app/main/palindrome/palindrome.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-palindrome',
   templateUrl: './palindrome.component.html',
-  styleUrls: ['./palindrome.component.scss']
+  styleUrls: ['./palindrome.component.scss'],
+  // encapsulation: ViewEncapsulation.None
 })
 export class PalindromeComponent {
 
@@ -15,10 +16,8 @@ export class PalindromeComponent {
   clicking(){
     this.palindrome = this.searchSentence.replaceAll(' ', '').toLowerCase()
     console.log("palindrome", this.palindrome);
-    
     this.palindromeCheck =this.searchSentence.split('').reverse().join().replaceAll(/[," "]/g, '').toLowerCase()
-    console.log('palindromeCheck', this.palindromeCheck);  
-    
+    console.log('palindromeCheck', this.palindromeCheck);      
     if(this.palindrome == this.palindromeCheck){
       this.cheking= true;
     } else if(this.palindrome != this.palindromeCheck){
